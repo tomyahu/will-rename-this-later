198cc052f591fe9b09424184ae7b8d163ffa58fa
tomimi <tomyahu@gmail.com>
2024-12-24 17:24:32 -0300

    added a remove log command

---
+++ src/commands/RemoveLogCommand.ts
@@ -0,0 +1,22 @@
+import { Controller } from "../Controller";
+import { Command } from "./Command";
+
+export class RemoveLogCommand extends Command {
+
+	constructor() {
+		super();
+		this._name = "remove_log";
+		this._description =  "Removes a log given an id"
+	}
+
+	// execute
+	// removes a log given an id
+	public execute(ctrl: Controller, args : string[]): void {
+		if( args.length != 1 )
+			return console.log("This command needs the id to delete the log")
+
+		let storage = ctrl.storage;
+		let logs = storage.logs
+		logs.splice( Number.parseInt( args[0] ), 1 )
+	}
+}
