b2ff6cb474c5a9ff965e8fd20459cdaf77355d33
tomimi <tomyahu@gmail.com>
2024-12-26 19:54:09 -0300

    added hp to characters

--- src/entities/Character.ts
+++ src/entities/Character.ts
@@ -2,11 +2,44 @@
 
 export class Character {
 	private _name : string;
+	private _max_hp : number;
+	private _hp : number;
+
 	constructor( name: string ) {
 		this._name = name;
+		this._max_hp = 1;
+		this._hp = this._max_hp;
+	}
+
+
+	// fullHeal
+	// sets current hp to max hp
+	public fullHeal() : void {
+		this._hp = this._max_hp;
+	}
+
+	
+	// damage
+	// damages the character
+	public damage( dmg : number ) : void {
+		this._hp -= dmg;
+		this._hp = Math.max( 0, this._hp );
+	}
+
+
+	// isAlive
+	// checks if the character has hp
+	public isAlive() : boolean {
+		return this._hp > 0;
 	}
 
 
 	// getters
-	get name() : string { return this._name }
+	get name() : string { return this._name; }
+	get max_hp() : number { return this._max_hp; }
+	get hp() : number { return this.hp; }
+
+	// setters
+	set max_hp( new_max_hp : number ) { this._max_hp = new_max_hp; }
+	set hp( new_hp : number ) { this._hp = new_hp; }
 }
--- src/factories/CharacterFactory.ts
+++ src/factories/CharacterFactory.ts
@@ -7,6 +7,9 @@ export class CharacterFactory {
 	// makes a new character from a dictionary
 	public static fromDictionary( dictionary ) : Character {
 		let character = new Character( dictionary._name )
+		if( "_max_hp" in dictionary ) character.max_hp = dictionary._max_hp;
+		if( "_hp" in dictionary ) character.hp = dictionary._hp;
+		
 		return character;
 	}
 }
